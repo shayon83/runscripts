2c5014ef25eecf7670a3973a6670038cd7ddd169
shayon83 <shayon83@gmail.com>
2016-07-22 00:57:40 -0700

    correct ip

--- src/install_rpm.sh
+++ src/install_rpm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp shayon-1-1.x86_64.rpm dpatadmin@10.212.64.142:/tmp
-ssh dpatadmin@10.212.64.142 sudo rpm -i /tmp/shayon-1-1.x86_64.rpm
+scp shayon-1-1.x86_64.rpm vagrant@10.0.0.56:/tmp
+ssh vagrant@10.0.0.56 sudo rpm -i /tmp/shayon-1-1.x86_64.rpm
